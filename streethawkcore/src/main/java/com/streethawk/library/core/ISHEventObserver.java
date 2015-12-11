a5ea3fb99bd9badb63e188b88159911905782918
Anurag Kondeya <anurag.kondeya@gmail.com>
2015-12-11 15:41:24 +1100

    Improved documentation for ISHEventObserver

--- streethawkcore/src/main/java/com/streethawk/library/core/ISHEventObserver.java
+++ streethawkcore/src/main/java/com/streethawk/library/core/ISHEventObserver.java
@@ -1,12 +1,12 @@
 package com.streethawk.library.core;
 
 /**
- * Created by anuragkondeya on 18/11/2015.
+ * Implement ISHEventObserver to get notified when your application is successfully registered with StreetHawk
  */
 public interface ISHEventObserver {
     /**
      * Function is called when install s registered with the device
-     * @param installId
+     * @param installId installid for your install
      */
     public void onInstallRegistered(String installId);
 }
